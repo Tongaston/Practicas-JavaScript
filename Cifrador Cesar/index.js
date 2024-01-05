4f7d46c0c4611b125d607d116e3d407bc49685d1
Tongaston <gaston.moure@gmail.com>
2024-01-05 16:16:41 +0100

    funcionalidad

--- Cifrador Cesar/index.js
+++ Cifrador Cesar/index.js
@@ -1,4 +1,4 @@
-const alfabeto = [
+const alphabet = [
   'A',
   'B',
   'C',
@@ -32,7 +32,42 @@ const range = document.getElementById('range');
 
 const shifMessage = () => {
   const wordArray = [...inputOriginal.value.toUpperCase()];
- 
+  printChar(0, wordArray);
+};
+
+const printChar = (currentLetterIndex, wordArray) => {
+  if (wordArray.length === currentLetterIndex) return;
+  inputOriginal.value = inputOriginal.value.substring(1);
+
+  const spanChar = document.createElement('span');
+  result.appendChild(spanChar);
+  animateChar(spanChar)
+    .then( () => {
+        const charSinCodificar = wordArray[currentLetterIndex];
+  spanChar.innerHTML = alphabet.includes(charSinCodificar)
+    ? alphabet[
+        (alphabet.indexOf(charSinCodificar) + parseInt(range.value)) %
+        alphabet.length
+      ]
+    : charSinCodificar;
+  printChar(currentLetterIndex + 1, wordArray);
+});
+    }
+
+  
+
+const animateChar = (spanChar) => {
+  let letterChange = 0;
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+        spanChar.innerHTML = alphabet[Math.floor(Math.random() * alphabet.length)]
+        letterChange++;
+        if(letterChange === 3) {
+            clearInterval(interval)
+            resolve();
+        }
+    }, 50);
+  });
 };
 
 const submit = (e) => {
