b55d107f1c733961da56c71f7fc3daefcf753275
Tongaston <gaston.moure@gmail.com>
2024-01-09 13:31:49 +0100

    estilos finales

---
+++ To do list/index.js
@@ -0,0 +1,49 @@
+// info date
+const dateNumber = document.getElementById('dateNumber');
+const dateDay = document.getElementById('dateDay');
+const dateMonth = document.getElementById('dateMonth');
+const dateYear = document.getElementById('dateYear');
+
+// Tasks Container
+const tasksContainer = document.getElementById('tasksContainer');
+
+const setDate = () => {
+  const date = new Date();
+  dateNumber.textContent = date.toLocaleString('es', { day: 'numeric' });
+  dateDay.textContent = date.toLocaleString('es', { weekday: 'long' });
+  dateMonth.textContent = date.toLocaleString('es', { month: 'short' });
+  dateYear.textContent = date.toLocaleString('es', { year: 'numeric' });
+};
+
+const addNewTask = (event) => {
+  event.preventDefault();
+  const { value } = event.target.taskText;
+  if (!value) return;
+  const task = document.createElement('div');
+  task.classList.add('task', 'roundBorder');
+  task.addEventListener('click', changeTaskState);
+  task.textContent = value;
+  tasksContainer.prepend(task);
+  event.target.reset();
+};
+
+const changeTaskState = (event) => {
+  event.target.classList.toggle('done');
+};
+
+const order = () => {
+  const done = [];
+  const toDo = [];
+  tasksContainer.childNodes.forEach((element) => {
+    element.classList.contains('done')
+      ? done.push(element)
+      : toDo.push(element);
+  });
+  return [...toDo, ...done];
+};
+
+const renderOrderedTasks = () => {
+    order().forEach(element => tasksContainer.appendChild(element))
+}
+
+setDate();
